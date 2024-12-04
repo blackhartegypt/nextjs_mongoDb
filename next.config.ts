8128ed71fa2a2142d022fb4379be20169dd14b09
blackhartegypt <blackhartegypt@gmail.com>
2024-12-04 13:25:11 +0300

    initial nextjs app commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// experimental:{
+//   turbopack:true,
+// }
+  
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  
 };
 
 export default nextConfig;
