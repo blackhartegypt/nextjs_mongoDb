294da4ef3412dbe69f1940762cf6fc7aad712fde
blackhartegypt <blackhartegypt@gmail.com>
2024-12-23 15:22:48 +0300

    edit next image configurations to accept images from firebase

--- next.config.ts
+++ next.config.ts
@@ -10,11 +10,20 @@
 
 // export default nextConfig;
 
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
